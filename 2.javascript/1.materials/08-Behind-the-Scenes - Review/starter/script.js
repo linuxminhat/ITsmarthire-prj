b6ba602cc022897f554cfb7461ffb0bd1475f6ee
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-02-25 19:37:14 +0700

    commit

--- 2.javascript/1.materials/08-Behind-the-Scenes - Review/starter/script.js
+++ 2.javascript/1.materials/08-Behind-the-Scenes - Review/starter/script.js
@@ -1,9 +1,10 @@
-'use strict';
-const myName = 'Jonas';
-if (myName === 'Jonas') {
-    console.log(`Jonas is a ${job}`);
-    const age = 2025 - 2003;
-    console.log(age);
-    const job = 'teacher';//Reference Error
-    console.log(x);
+let jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 29,
 }
+const marriedJessica = jessica;
+marriedJessica.lastName = 'David';
+console.log('Before:', jessica);
+console.log('After:', marriedJessica);
+jessica = { childrens: 2 };
