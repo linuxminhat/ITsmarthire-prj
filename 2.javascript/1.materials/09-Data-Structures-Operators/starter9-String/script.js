596c644e0f98ca2d64d2bc8e290f212f7810d0fa
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-03-02 23:46:33 +0700

    done

---
+++ 2.javascript/1.materials/09-Data-Structures-Operators/starter9-String/script.js
@@ -0,0 +1,5 @@
+'use strict';
+const myFirstSalary = ('Luong : 10.000.000 VND va Thuong : 1000.000 VND');
+console.log(myFirstSalary);
+console.log(myFirstSalary.replace('VND', 'USD'));
+console.log(myFirstSalary.replaceAll('VND', 'USD'));
