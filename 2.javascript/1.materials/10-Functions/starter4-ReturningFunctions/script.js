2f9e3d865ac17dbdbc1366b1ac362dd5f943acb2
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-03-03 22:02:29 +0700

    done node

--- 2.javascript/1.materials/10-Functions/starter4-ReturningFunctions/script.js
+++ 2.javascript/1.materials/10-Functions/starter4-ReturningFunctions/script.js
@@ -1,9 +1,21 @@
-'use strict';
-const greet = function (greeting) {
-    return function (name) {
-        console.log(`${greeting} ${name}`);
-    };
-};
-const greeterHey = greet('Hey');
-greeterHey('Dang Nhat Minh');
-greeterHey('Nguyen Duc Chung');
+
+// 'use strict';
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+//     book(flightNumb, name) {
+//         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNumb}`);
+//         this.bookings.push({ flight: `${this.iataCode} ${flightNumb}`, name });
+//     },
+// };
+// lufthansa.book(2810, 'Jonas');
+// lufthansa.book(635, 'John');
+// console.log(lufthansa);
+// const euroWings = {
+//     name: 'EuroWings',
+//     iataCode: 'EW',
+//     bookings: [],
+// };
+// const book = lufthansa.book;
+// book.call(euroWings, 299, 'DangMin ');
