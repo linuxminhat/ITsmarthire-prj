82ea0af268a0020bc706a132cb72b57156df44fb
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-03-05 13:32:54 +0700

    commit

--- 2.javascript/1.materials/10-Functions/starter4-ReturningFunctions/script.js
+++ 2.javascript/1.materials/10-Functions/starter4-ReturningFunctions/script.js
@@ -1,21 +1,34 @@
+'use strict';
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNumb, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNumb}`);
+        this.bookings.push({ flight: `${this.iataCode} ${flightNumb}`, name })
+    },
+};
+lufthansa.book(2810, 'Dang');
+lufthansa.book(635, 'Jonas');
+console.log(lufthansa);
+const euroWings = {
+    name: 'EuroWings',
+    iataCode: 'EW',
+    bookings: [],
 
-// 'use strict';
-// const lufthansa = {
-//     airline: 'Lufthansa',
-//     iataCode: 'LH',
-//     bookings: [],
-//     book(flightNumb, name) {
-//         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNumb}`);
-//         this.bookings.push({ flight: `${this.iataCode} ${flightNumb}`, name });
-//     },
-// };
-// lufthansa.book(2810, 'Jonas');
-// lufthansa.book(635, 'John');
-// console.log(lufthansa);
-// const euroWings = {
-//     name: 'EuroWings',
-//     iataCode: 'EW',
-//     bookings: [],
-// };
-// const book = lufthansa.book;
-// book.call(euroWings, 299, 'DangMin ');
+}
+const swiss = {
+    name: 'SwissWings',
+    iataCode: 'SW',
+    bookings: [],
+}
+const book = lufthansa.book;
+book.call(euroWings, 23, 'Sarah');//Chi co function moi goi duoc call apply hay bind
+
+lufthansa.planes = 2810; //default setting 
+lufthansa.buyPlanes = function () {
+    console.log(this);
+    this.planes++;
+    console.log(`Our planes now is + ${this.planes}`);
+}
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlanes);
