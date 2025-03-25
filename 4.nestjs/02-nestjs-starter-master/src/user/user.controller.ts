effb91b11429e8a2df001ddf836ebc83e8730ab2
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-03-26 00:40:32 +0700

    nest/controller

---
+++ 4.nestjs/02-nestjs-starter-master/src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Delete } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+  @Get()
+  findAll(): string {
+    return "This action return all user";
+  }
+
+  @Delete("/by-id")
+  findById(): string {
+    return "This action will delete a user by id";
+  }
+}
