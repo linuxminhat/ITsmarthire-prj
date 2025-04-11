24b47ce17dc903ee16aac348a07c83e0d9dd2cc7
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-04-11 19:45:02 +0700

    customize message decorator

--- be/be-nestjs/src/main.ts
+++ be/be-nestjs/src/main.ts
@@ -25,6 +25,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth-guards';
+import { TransformInterceptor } from './core/transform.interceptor';
 require('dotenv').config();
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -35,6 +36,8 @@ async function bootstrap() {
     app.setBaseViewsDir(join(__dirname, "..", "views"));//view 
     app.setViewEngine("ejs");
     app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
     //config cors
     app.enableCors(
         {
