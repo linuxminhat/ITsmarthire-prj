4833b4562766c01fc622e8477a9afef747146bef
linuxminhat <dangnhatminhdhbk@gmail.com>
2025-04-13 22:26:36 +0700

    first

--- be/be-nestjs/src/app.module.ts
+++ be/be-nestjs/src/app.module.ts
@@ -17,6 +17,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 import { CompaniesModule } from './companies/companies.module';
+import { JobsModule } from './jobs/jobs.module';
 
 
 @Module({
@@ -43,6 +44,7 @@ import { CompaniesModule } from './companies/companies.module';
     UsersModule,
     AuthModule,
     CompaniesModule,
+    JobsModule,
 
   ],
 
